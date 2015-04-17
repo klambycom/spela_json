664b317fbc45eab969ce374bfe0deebbd83bff14
klambycom <christian@klamby.com>
2015-04-17 20:47:09 +0200

    Add end time to example

--- example.js
+++ example.js
@@ -6,28 +6,33 @@ let file = {
     '1': {
       type: 'file',
       file: '/alien_phaser.wav',
-      start: 0
+      start: 0,
+      end: 1.399
     },
     '2': {
       type: 'file',
       file: '/car.wav',
-      start: 1.5
+      start: 1.5,
+      end: 1.2935833333
     },
     '3': {
       type: 'file',
       file: '/crumple_paper.wav',
-      start: 3
+      start: 3,
+      end: 2.69875
     },
     '4': {
       type: 'file',
       file: '/mbira.wav',
       start: 5,
+      end: 47.3308125,
       rate: 4
     },
     '5': {
       type: 'file',
       file: '/surround.wav',
       start: 10,
+      end: 15.9999,
       rate: 3
     }
   }
@@ -38,28 +43,33 @@ let file2 = {
     '1': {
       type: 'file',
       file: '/alien_phaser.wav',
-      start: -10
+      start: -10,
+      end: 1.399
     },
     '2': {
       type: 'fille',
       file: '/car.wav',
-      start: 1.5
+      start: 1.5,
+      end: 1.2935833333
     },
     '3': {
       type: 'file',
       file: '/crumple_paper.wav',
-      start: 3
+      start: 3,
+      end: 2.69875
     },
     '4': {
       type: 'file',
       file: '/mbira.wav',
       start: 5,
+      end: 47.3308125,
       rate: 4
     },
     '5': {
       type: 'file',
       file: '/surround.wav',
       start: 10,
+      end: 15.9999,
       rate: 3
     }
   }
